6a1c74437702dbafcf867f81abab9335ad7f939d
camirali64-sketch <camirali64@gmail.com>
2026-07-27 11:23:02 +0300

    vite

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
+                  'resources/css/login.css',
                 'resources/css/registration.css',
                 'resources/css/subuser.dashboard.css',
                 'resources/css/subuser.index.css',
